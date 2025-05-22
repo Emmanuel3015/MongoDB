5837194076c6f2fc00f5a47c56ea72487bf58d04
Emmanuel <emmanuelmwangi260@gmail.com>
2025-05-22 09:29:52 +0300

    modified file

--- app/routes/products.jsx
+++ app/routes/products.jsx
@@ -10,14 +10,14 @@ export async function loader() {
 const Products = ({ loaderData }) => {
   return (
     <main className="max-w-6xl mx-auto">
-      <h1 className="font-bold text-4xl text-red-500">Products</h1>
+      <h1 className="font-bold text-4xl text-red-500 mt-10">Products</h1>
       <ul className="mt-6 space-y-4 grid grid-cols-3 gap-4">
-        {loaderData.map((product) => (
-          <li key={product._id} className="p-4 border rounded shadow">
-            <h2 className="text-xl font-semibold">{product.title}</h2>
-            <p className="text-gray-700">{product.price}</p>
-            <p className="text-green-600 font-bold">${product.quantity}</p>
-            <img src={product.image} alt={product.title} />
+        {loaderData.map((item) => (
+          <li key={item._id} className="p-4 border rounded shadow">
+            <h2 className="text-xl font-semibold">{item.title}</h2>
+            <p className="text-gray-700">{item.price}</p>
+            <p className="text-green-600 font-bold">${item.quantity}</p>
+            <img src={item.image} alt={item.title} className="h-100" />
           </li>
         ))}
       </ul>
